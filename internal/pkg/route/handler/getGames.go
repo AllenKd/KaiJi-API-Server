db39612e8d52433f0b1ee6cb4ca9230299399a9a
Allen <kaodondon7@gmail.com>
2020-10-04 04:56:49 +0800

    make query flexible

--- internal/pkg/route/handler/getGames.go
+++ internal/pkg/route/handler/getGames.go
@@ -2,52 +2,88 @@ package handler
 
 import (
 	"KaiJi-API-Server/internal/pkg/db"
+	"KaiJi-API-Server/internal/pkg/db/collection"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-// TODO: make query parameter flexible
-type GetGamesReq struct {
-	GameType string
-	Begin string
-	End string
+type GetGamesFilter struct {
+	GameType string               `bson:"game_type,omitempty"`
+	GameTime map[string]time.Time `bson:"game_time,omitempty"`
+}
+
+type GetGamesOption struct {
+	Limit  int
+	Offset int
+	SortBy string
+	Desc   bool
+}
+
+var parseOrder = func(b bool) int {
+	if b {
+		return -1
+	} else {
+		return 1
+	}
 }
 
 func GetGames(c *gin.Context) {
 	log.Debug("getting games")
-	req := GetGamesReq{
-		GameType: c.Query("game-type"),
-		Begin: c.Query("begin"),
-		End: c.Query("end"),
-	}
 
-	begin, err := time.Parse("20060102", req.Begin)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+	// parse query parameters
+	gameType := c.Query("game-type")
+	begin, _ := strconv.ParseInt(c.Query("begin"), 10, 64)
+	end, _ := strconv.ParseInt(c.Query("end"), 10, 64)
+	limit, _ := strconv.ParseInt(c.Query("limit"), 10, 64)
+	offset, _ := strconv.ParseInt(c.Query("offset"), 10, 64)
+	sortBy := c.DefaultQuery("sort-by", "_id")
+	desc, _ := strconv.ParseBool(c.Query("desc"))
+
+	// construct query filter
+	req := new(GetGamesFilter)
+	if gameType != "" {
+		req.GameType = gameType
+	}
+	req.GameTime = map[string]time.Time{}
+	if begin > 0 {
+		req.GameTime["$gte"] = time.Unix(begin, 0)
+	}
+	if end > 0 {
+		req.GameTime["$lte"] = time.Unix(end, 0)
+	}
+	var filter bson.M
+	b, _ := bson.Marshal(req)
+	if err := bson.Unmarshal(b, &filter); err != nil {
+		log.Error("fail to unmarshal filter: ", err.Error())
+		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	end, err := time.Parse("20060102", req.End)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	filter := bson.M{
-		"game_type": req.GameType,
-		"game_time": bson.M{
-			"$gte": begin,
-			"$lte": end,
+	// construct query options
+	option := &options.FindOptions{
+		Limit: &limit,
+		Skip:  &offset,
+		Sort: bson.M{
+			sortBy: parseOrder(desc),
 		},
 	}
-	documents, err := db.New().GetGames(filter)
+
+	documents, err := db.New().GetGames(filter, option)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	c.JSON(http.StatusOK, documents)
+	c.JSON(http.StatusOK, struct {
+		Count int64                   `json:"count"`
+		Games []collection.SportsData `json:"games"`
+	}{
+		Count: db.New().CountGames(filter),
+		Games: documents,
+	})
 }
